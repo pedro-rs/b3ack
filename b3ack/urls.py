67d3b766a8626f0bcc0fbf410a5f31443aa2e75f
pedro-rs <pedro.rsr@outlook.com>
2022-11-18 23:09:42 -0300

    Users creating/authenticating basics

--- b3ack/urls.py
+++ b3ack/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("search", views.search, name="search"),
-    path("company/<str:cod>", views.company, name="company"),
-    # path("<str:name>", views.greet, name="greet")
+    path("", views.index_view, name="index"),
+    path("login", views.login_view, name="login"),
+    path("register", views.register_view, name="register"),
+    path("logout", views.logout_view, name="logout"),
+    path("search", views.search_view, name="search"),
+    path("company/<str:cod>", views.company_view, name="company"),
 ]
--- b3ack/views.py
+++ b3ack/views.py
@@ -1,21 +1,78 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from b3ack.utils.b3api import B3api
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World")
+def index_view(request):
+    if not request.user.is_authenticated:
+        return HttpResponse("Você não está logado!")
+    else:
+        return HttpResponse(f"Logado como {request.user}")
 
-def search(request):
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "b3ack/login.html", {
+                "message": "Credenciais incorretas!"
+            })
+
+    else:
+        return render(request, "b3ack/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "b3ack/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "b3ack/register.html", {
+                "message": "Email address or username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+
+    else:
+        return render(request, "b3ack/register.html")
+
+
+def search_view(request):
     api = B3api()
-    # quotes = api.get_quotes(id=1)
-    print(api.data)
 
     return render(request, "b3ack/search.html", {
         "data": api.data.values()
     })
 
-def company(request, cod):
+def company_view(request, cod):
     api = B3api()
     company = api.data[cod]
     quotes = api.get_quotes(cod=cod)
@@ -23,10 +80,4 @@ def company(request, cod):
     return render(request, "b3ack/company.html", {
         "company": company,
         "quotes": quotes
-    })
-
-
-def greet(request, name: str):
-    return render(request, "b3ack/greet.html", {
-        "name": name.capitalize()
     })
